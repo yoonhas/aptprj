ed0dc5647f3eb3ef8c0b8d198dc01c7c0cf99496
junlee91 <jlee757@uic.edu>
2018-02-01 21:43:49 -0600

    postgres implemented

--- apt_community/settings.py
+++ apt_community/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -75,10 +74,14 @@ WSGI_APPLICATION = 'apt_community.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'aptdb',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
